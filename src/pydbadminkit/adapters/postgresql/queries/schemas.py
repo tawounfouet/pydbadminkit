2a1f029eb914c48a67853a43f166e9667316d8ed
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-21 14:39:22 +0200

    fix(catalog): avoid psycopg percent parsing and tighten CLI errors

--- src/pydbadminkit/adapters/postgresql/queries/schemas.py
+++ src/pydbadminkit/adapters/postgresql/queries/schemas.py
@@ -6,8 +6,8 @@ GET_SCHEMA_QUERY_ID = "PG_GET_SCHEMA"
 _SYSTEM_SCHEMA_PREDICATE = """
 (
     n.nspname IN ('pg_catalog', 'information_schema', 'pg_toast')
-    OR n.nspname LIKE 'pg_temp_%'
-    OR n.nspname LIKE 'pg_toast_temp_%'
+    OR n.nspname ~ '^pg_temp_'
+    OR n.nspname ~ '^pg_toast_temp_'
 )
 """
 
