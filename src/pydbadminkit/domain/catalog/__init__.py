f5d0819f53d674301849cf657076bbaaf829fa59
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-21 14:45:35 +0200

    feat(catalog): extend public catalog contracts for views and indexes

--- src/pydbadminkit/domain/catalog/__init__.py
+++ src/pydbadminkit/domain/catalog/__init__.py
@@ -2,6 +2,7 @@
 
 from pydbadminkit.domain.catalog.column import ColumnInfo
 from pydbadminkit.domain.catalog.database import DatabaseInfo
+from pydbadminkit.domain.catalog.index import IndexDescription, IndexInfo
 from pydbadminkit.domain.catalog.schema import SchemaInfo
 from pydbadminkit.domain.catalog.server import ServerInfo
 from pydbadminkit.domain.catalog.table import (
@@ -11,15 +12,21 @@ from pydbadminkit.domain.catalog.table import (
     TableInfo,
     TableKind,
 )
+from pydbadminkit.domain.catalog.view import ViewDescription, ViewInfo, ViewKind
 
 __all__ = [
     "ColumnInfo",
     "ConstraintInfo",
     "ConstraintType",
     "DatabaseInfo",
+    "IndexDescription",
+    "IndexInfo",
     "SchemaInfo",
     "ServerInfo",
     "TableDescription",
     "TableInfo",
     "TableKind",
+    "ViewDescription",
+    "ViewInfo",
+    "ViewKind",
 ]
