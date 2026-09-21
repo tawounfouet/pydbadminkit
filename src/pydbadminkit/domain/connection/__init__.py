5a6e475f18636d64d1e0142fa6350d720c1dc466
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-21 13:28:51 +0200

    feat(core): add initial domain primitives and catalog models

---
+++ src/pydbadminkit/domain/connection/__init__.py
@@ -0,0 +1,6 @@
+"""Connection-related domain primitives."""
+
+from pydbadminkit.domain.connection.profile import ConnectionProfileName
+from pydbadminkit.domain.connection.secrets import SecretReference
+
+__all__ = ["ConnectionProfileName", "SecretReference"]
